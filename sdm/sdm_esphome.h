a11d9199c11b2958b9a691735a2ab81a6d1cd2e2
José Ferreira <51744712+zcsrf@users.noreply.github.com>
2020-01-28 12:01:32 +0000

    ADD: Required SDM230 values as Sensors
    The initial fork didn't have that many values being read, added all our initially required values and also changed registers from SDM630 to SDM230.

--- sdm/sdm_esphome.h
+++ sdm/sdm_esphome.h
@@ -7,9 +7,17 @@ public:
   Sensor *voltage_sensor = new Sensor();
   Sensor *current_sensor = new Sensor();
   Sensor *power_sensor = new Sensor();
+  Sensor *apparent_power_sensor = new Sensor();
+  Sensor *power_factor_sensor = new Sensor();
+  Sensor *phase_angle_sensor = new Sensor();
+  Sensor *line_frequency_sensor = new Sensor();
   Sensor *energy_import_sensor = new Sensor();
+  
+  // remove when no longer debugging the implementation
+  Sensor *read_error_count_sensor = new Sensor();
 
-  SDMComponent() : PollingComponent(15000) {}
+  // Adjust your polling component to your needs, value is in ms, 5 seconds = 5000 ms
+  SDMComponent() : PollingComponent(5000) {}
 
   void setup() override {
     ESP_LOGD("SDM", "Setup completed");
@@ -17,24 +25,42 @@ public:
 
   void update() override {
     ESP_LOGD("SDM", "Start reading");
-    float voltage1 = sdm.readVal(SDM630_VOLTAGE1);
-    float voltage2 = sdm.readVal(SDM630_VOLTAGE2);
-    float voltage3 = sdm.readVal(SDM630_VOLTAGE3);
-    ESP_LOGD("SDM", "Voltages %f %f %f", voltage1, voltage2, voltage3);
-    voltage_sensor->publish_state((voltage1 + voltage2 + voltage3)/3);
-
-    float current = sdm.readVal(SDM630_CURRENTSUM);
+    // Voltage Reading
+    float voltage = sdm.readVal(SDM230_VOLTAGE);
+    voltage_sensor->publish_state((voltage);
+    
+    //  Current Reading
+    float current = sdm.readVal(SDM230_CURRENT);
     current_sensor->publish_state(current);
 
-    float power = sdm.readVal(SDM630_POWERTOTAL);
+    // Power Reading
+    float power = sdm.readVal(SDM230_POWER);
     power_sensor->publish_state(power);
 
-    float import1 = sdm.readVal(SDM630_IMPORT1);
-    float import2 = sdm.readVal(SDM630_IMPORT2);
-    float import3 = sdm.readVal(SDM630_IMPORT3);
-    ESP_LOGD("SDM", "Imports %f %f %f", import1, import2, import3);
-    energy_import_sensor->publish_state(import1 + import2 + import3);
+    // Apparent Power Reading
+    float apparent_power = sdm.readVal(SDM230_ACTIVE_APPARENT_POWER);
+    apparent_power_sensor->publish_state(apparent_power);        
+    
+    // Power Factor reading                              
+    float power_factor = sdm.readVal(SDM230_POWER_FACTOR);
+    power_factor_sensor->publish_state(power_factor);      
+                                  
+    // Phase angle reading
+    float phase_angle = sdm.readVal(SDM230_PHASE_ANGLE);
+    phase_angle_sensor->publish_state(phase_angle);     
+    
+    // Line frequency reading
+    float frequency = sdm.readVal(SDM230_FREQUENCY);
+    line_frequency_sensor->publish_state(frequency);     
 
+    // Total active energy reading
+    float total_active_energy = sdm.readVal(SDM230_TOTAL_ACTIVE_ENERGY);
+    energy_import_sensor->publish_state(total_active_energy);
+
+    // Number of errros while reading, remove when no longer debugging the implementation
+    int error_count = sdm.getErrCount();
+    read_error_count_sensor->publish_state(error_count);
+                                  
     ESP_LOGD("SDM", "End reading");
   }
 };
